885a8098889fff93c4319997a4ef72d4534ec01e
nico1ina <nicolinalarsson1@gmail.com>
2024-04-23 11:06:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React + TypeScript + Vite
+
+---
+
+## Run yarn install
+
+## Then run yarn dev to open project on http://localhost:5173
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Calculator from "./Calculator";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="app-container">
+        <h1 className="title">Nicolinas Calculator</h1>
+        <Calculator />
+      </div>
+    </>
+  );
+}
